b1dc153cef043078f925ab028ef723401654163d
lnadenau <lukas.nadenau@inform-software.com>
2025-04-15 00:08:05 +0200

    Umgebungsvariablen auskommentiert

--- GoogleSearchMCP.Tests/Tests.cs
+++ GoogleSearchMCP.Tests/Tests.cs
@@ -6,8 +6,9 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        Environment.SetEnvironmentVariable("GOOGLE_API_KEY", "AIzaSyCDhsuGCzVzF1TfYch9_jktw9o95UHSGDY");
-        Environment.SetEnvironmentVariable("GOOGLE_SEARCH_ENGINE_ID", "c3bfee9f1a29c4c27");
+        // Enable the following lines to set up your environment variables for testing
+        // Environment.SetEnvironmentVariable("GOOGLE_API_KEY", "AIzaSyCDhsuGCzVzF1TfYch9_jktw9o95UHSGDY");
+        // Environment.SetEnvironmentVariable("GOOGLE_SEARCH_ENGINE_ID", "c3bfee9f1a29c4c27");
     }
     
     [Test]
